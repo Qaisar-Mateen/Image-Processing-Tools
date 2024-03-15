208e2691decf7f70f69344b3597b82bd601560da
Qaisar Mateen <qaisarmateen313@gmail.com>
2024-03-15 16:10:37 +0500

    Create source.py

---
+++ source.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+import cv2
+import os
+import numpy as np
+from PIL import Image
+from tkinter import filedialog, messagebox
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('dark-blue')
