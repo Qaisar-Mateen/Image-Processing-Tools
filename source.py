43b53717e0f8d711d27978cafbe9033240031d34
Qaisar Mateen <qaisarmateen313@gmail.com>
2024-03-27 22:11:04 +0500

    Update source.py

--- source.py
+++ source.py
@@ -597,3 +597,8 @@ def basicLayout():
         app.geometry('%dx%d'% (width//1.3, (height//1.3)+40))
         app.columnconfigure(0, weight=1)
         app.rowconfigure(1, weight=1)
+
+        # upper frame for images
+        upperFr = ctk.CTkFrame(app, corner_radius=20)
+        upperFr.grid(pady=(20, 15), padx=30, row=0, column=0, sticky='new',)
+        upperFr.columnconfigure((0,8), weight=1)
