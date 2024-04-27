f7f9119785ea4ed78bfa5f88a360d5d946294bb7
Qaisar-Mateen <qaisarmateen313@gmail.com>
2024-04-27 16:00:36 +0500

    Refactor import statements in source.py to include time module

--- source.py
+++ source.py
@@ -1,13 +1,9 @@
 import customtkinter as ctk
-import cv2
-import os
-import threading
-import numpy as np
+import cv2, os, threading, numpy as np, time
 from PIL import Image
 from tkinter import filedialog, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import time
 
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('dark-blue')
